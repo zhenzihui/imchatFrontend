61ce592f91f55741521fdfb179a5d672a01480dc
zhenzihui <578079901@qq.com>
2020-10-30 17:56:03 +0800

    first commot

---
+++ src/components/MessageList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {IMessage, IUserListProps} from "../Interface";
+import Axios from "axios";
+import {host} from "../Common";
+
+const UserList = (props: IUserListProps) => {
+
+    const source = new EventSource(host + "/api/user/message/2");
+
+    source.onmessage = (messageEvent: MessageEvent<IMessage>) => {
+
+    }
+
+    return (
+        <div>
+            <ul>
+                {props.users.map(user => {
+                    return <li>{user.name}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default UserList
