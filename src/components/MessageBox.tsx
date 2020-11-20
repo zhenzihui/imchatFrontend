bc560c1a00112b14b569c8860c65117d75f62961
zhenzihui <578079901@qq.com>
2020-11-20 17:21:07 +0800

    fix: chatbox

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import {useComponentStyles, useStyles} from "../Common";
-import {Button, Grid, InputBase} from "@material-ui/core";
+import {Button, Fab, Grid, InputBase, TextField} from "@material-ui/core";
 
 export default function () {
     const componentStyles = useComponentStyles();
     const classes = useStyles();
     return (
 
-    <Grid container spacing={0} className={classes.stickToBottom}>
-        <Grid item xs>
+    <Grid container spacing={1} className={classes.stickToBottom}>
+        <Grid item xs={9}>
+            <TextField
+                variant="standard"
+                fullWidth
+                multiline
+            />
         </Grid>
-        <Grid item xs={10}>
-            <InputBase className={componentStyles.root} fullWidth endAdornment={<Button>Send</Button>}/>
-        </Grid>
-        <Grid item xs>
+        {/*<Grid item xs={10}>*/}
+        {/*    <InputBase className={componentStyles.root} fullWidth endAdornment={<Button>Send</Button>}/>*/}
+        {/*</Grid>*/}
+        <Grid item xs={3}>
+            <Button variant="contained" color="primary">
+                Send
+            </Button>
         </Grid>
     </Grid>
     )
